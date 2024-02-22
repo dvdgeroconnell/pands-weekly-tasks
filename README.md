e6a08186a15f3d7a3019d1b0f1e2791c33c83056
David O'Connell <dvdgeroconnell@gmail.com>
2024-02-22 10:11:58 +0000

    Weekly Task 05 - first commit

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # pands-weekly-tasks
-Coursework repository.
-Weekly Task 02 - bank.py
-Weekly Task 03 - accounts.py
-Weekly Task 04 - collatz.py
+This is the coursework repository.
+
+## Weekly Task 02 - bank.py
+## Weekly Task 03 - accounts.py
+## Weekly Task 04 - collatz.py
+
+## Weekly Task 05 - weekday.py
+This is a program to output whether or not the current day is a weekday.
+If not a weekday, it also confirms that it is a weekend day.
+If neither, an error message is printed.
---
+++ weekday.py
@@ -0,0 +1,37 @@
+# weekday.py
+# Week 05 assignment
+
+# Program to output whether or not the current day is a weekday.
+# If not a weekday, it also confirms that it is a weekend day.
+# If neither, an error message is printed.
+
+# Author: David O'Connell
+
+# The datetime module has many methods to return information about the date object.
+# Reference - https://www.w3schools.com/python/python_datetime.asp
+
+# The method we require to return the current day is in the datetime module.
+import datetime
+
+# Define the weekdays and weekend days as tuples.
+weekday = ("Monday","Tuesday","Wednesday","Thursday","Friday")
+weekend = ("Saturday","Sunday")
+
+
+today = datetime.datetime.now()
+
+# The method strftime() formats date objects into readable strings.
+# It takes one parameter to specify the format of the returned string.
+# %A is the day of the week. Reference - W3schools.
+
+# Check if today is a weekday.
+if (today.strftime("%A")) in weekday:
+    print("Yes, unfortunately today is a weekday.")
+
+# If not a weekday, double check that it is a weekend day.
+elif (today.strftime("%A")) in weekend:
+    print("It is the weekend, yay!")
+
+# If neither, print an error message. We should never get here.
+else:
+    print("I don't know what day it is!")
